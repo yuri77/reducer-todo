f32ee47e320f43aa19c8d4c173f7e43cf370bd7c
yurika sakae <yurika.sun@gmail.com>
2019-08-13 16:58:10 -0700

    addItem complete WIP:toggle

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,15 +1,28 @@
 import React, { useReducer } from "react";
 import { initialState, todoReducer } from "./reducers/todoReducer";
-
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
+import { TodoContext } from "./contexts/TodoContext";
 import "./App.css";
 
 function App() {
   const [state, dispatch] = useReducer(todoReducer, initialState);
 
+  function addItem(task) {
+    dispatch({ type: "ADD_TASK", payload: task });
+  }
+
+  console.log("App-state", state);
   return (
-    <div className="App">
-      <h1>Todo List: </h1>
-    </div>
+    <TodoContext.Provider value={state.todoItems}>
+      <div className="App">
+        <h1>Todo List: </h1>
+        <TodoForm addItem={addItem} />
+        {state.todoItems.map(item => (
+          <TodoList key={item.id} item={item} />
+        ))}
+      </div>
+    </TodoContext.Provider>
   );
 }
 
